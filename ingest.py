4d914a5f984b2fdccd01079b37999bd2b0335c40
su77ungr <69374354+su77ungr@users.noreply.github.com>
2023-05-11 22:39:31 +0200

    Adding checks and variable path to data source
    ## enables to run python random_path/ to ingest // or 'python random_path/ y' to purge existing db

--- ingest.py
+++ ingest.py
@@ -1,49 +1,37 @@
 import shutil
+import os
+import sys
 from langchain.document_loaders import TextLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.vectorstores import Qdrant
 from langchain.embeddings import LlamaCppEmbeddings
-from sys import argv
-import os
 
-def main():
-    # Check if there is an existing db, and if so delete or add to it.
-    # supports command line "python ./ingest.py y" or "python ./ingest.py n"
-    # y=new db, n=existing db
+## enables to run python random_path/ to ingest // or 'python random_path/ y' to purge existing db 
+def main(sources_directory, cleandb):
     db_dir = "./db"
-    if os.path.exists("./db"):
-        if len(argv) > 1:
-            cleandb = argv[1]
-        else:
-            # default set to n // keep db 
-            cleandb = "n"
-
-        if cleandb.lower() == 'y':
+    if os.path.exists(db_dir):
+        
+        if cleandb.lower() == 'y' or (cleandb == 'n' and input("\nDelete current database?(Y/N): ").lower() == 'y'):
             print('Deleting db...')
             shutil.rmtree(db_dir)
-            os.makedirs(db_dir)
-        
-    # Load document and split in chunks
-    for root, dirs, files in os.walk("source_documents"):
+        elif cleandb.lower() == 'n':
+            print('Adding to db...')
+
+    for root, dirs, files in os.walk(sources_directory):
         for file in files:
             if file.endswith(".txt"):
                 loader = TextLoader(os.path.join(root, file), encoding="utf8")
-
+    loader = TextLoader(os.path.join(root, file), encoding="utf8")
+    
     documents = loader.load()
     text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=50)
     texts = text_splitter.split_documents(documents)
-    # Create embeddings
     llama = LlamaCppEmbeddings(model_path="./models/ggml-model-q4_0.bin")
-
-    qdrant = Qdrant.from_documents(
-    texts, llama, path="./db",  # Local mode with in-memory storage only
-    collection_name="test",
-)
-
-    qdrant
+    qdrant = Qdrant.from_documents(texts, llama, path="./db", collection_name="test")
     qdrant = None
     print("Indexed ", len(texts), " documents in Qdrant")
 
-
 if __name__ == "__main__":
-    main()
+    sources_directory = sys.argv[1]
+    cleandb = sys.argv[2] if len(sys.argv) > 2 else 'n'
+    main(sources_directory, cleandb)
