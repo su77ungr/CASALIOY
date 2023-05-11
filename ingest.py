ea592264603d06243f8a652747bb5ba120cc4a0d
su77ungr <69374354+su77ungr@users.noreply.github.com>
2023-05-11 21:41:48 +0200

    Update ingest.py

--- ingest.py
+++ ingest.py
@@ -11,36 +11,39 @@ def main():
     # supports command line "python ./ingest.py y" or "python ./ingest.py n"
     # y=new db, n=existing db
     db_dir = "./db"
-    
-    if os.path.exists(db_dir):
-        cleandb = input("\nDelete current database? (Y/N): ")
+    if os.path.exists("./db"):
+        if len(argv) > 1:
+            cleandb = argv[1]
+        else:
+            # default set to n // keep db 
+            cleandb = "n"
+
         if cleandb.lower() == 'y':
             print('Deleting db...')
             shutil.rmtree(db_dir)
-        elif cleandb.lower() == 'n':
-            print('Adding to existing db...')
-        else:
-            print('No option selected!')
+            os.makedirs(db_dir)
         
-    os.makedirs(db_dir, exist_ok=True)
     # Load document and split in chunks
     for root, dirs, files in os.walk("source_documents"):
         for file in files:
             if file.endswith(".txt"):
                 loader = TextLoader(os.path.join(root, file), encoding="utf8")
+
     documents = loader.load()
     text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=50)
     texts = text_splitter.split_documents(documents)
     # Create embeddings
     llama = LlamaCppEmbeddings(model_path="./models/ggml-model-q4_0.bin")
 
-
     qdrant = Qdrant.from_documents(
     texts, llama, path="./db",  # Local mode with in-memory storage only
     collection_name="test",
 )
+
     qdrant
     qdrant = None
+    print("Indexed ", len(texts), " documents in Qdrant")
+
 
 if __name__ == "__main__":
     main()
