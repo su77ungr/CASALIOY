4a146ce5b03041b53260e61b9e32edab246e05e3
alxspiker <alxtheprogrammer@gmail.com>
2023-05-13 14:53:58 -0600

    Code cleanup for commit

--- gui.py
+++ gui.py
@@ -27,37 +27,21 @@ with st.sidebar:
     add_vertical_space(5)
     st.write('Made with ❤️ by [su77ungr/CASALIOY](https://github.com/alxspiker/CASALIOY)')
 
-# Generate empty lists for generated and past.
-## generated stores AI generated responses
 if 'generated' not in st.session_state:
     st.session_state['generated'] = ["I can help you answer questions about the documents you have ingested into the vector store."]
-## past stores User's questions
+
 if 'past' not in st.session_state:
     st.session_state['past'] = ['Hi, what can you help me with!']
 
-# Layout of input/response containers
 input_container = st.container()
 colored_header(label='', description='', color_name='blue-30')
 response_container = st.container()
 
-# User input
-## Function for taking user provided prompt as input
-#def get_text():
-    #return input_text
-
-## Applying the user input box
-#with input_container:
-    #user_input = get_text()
-
-# Response output
-## Function for taking user prompt as input followed by producing AI generated responses
 def generate_response(prompt):
     if st.session_state['generated']:
         for i in range(len(st.session_state['generated'])):
             message(st.session_state["past"][i], is_user=True, key=str(i) + '_user')
             message(st.session_state["generated"][i], key=str(i))
-    #if prompt.strip() != "":
-    
     if st.session_state.running==False and prompt.strip() != "":
         st.session_state.running = True
         st.session_state.past.append(prompt)
@@ -67,14 +51,8 @@ def generate_response(prompt):
         st.session_state.generated.append(response)
         message(response)
         st.session_state.running = False
-    #st.session_state.rmessage = response
-    #return response
     st.text_input("You: ", "", key="input", disabled=st.session_state.running)
-#if user_input:
-    #response = generate_response(user_input)
-    #st.session_state.past.append(user_input)
-    #st.session_state.generated.append(response)
+
 with st.container():
     with st.form("my_form", clear_on_submit=True):
-    ## Conditional display of AI generated responses as a function of user provided prompts
         st.form_submit_button('SUBMIT', on_click=generate_response(st.session_state.input), disabled=st.session_state.running)
