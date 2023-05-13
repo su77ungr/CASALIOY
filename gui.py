d5499e6d30b501e600786e6daea25859b0d573ea
alxspiker <alxtheprogrammer@gmail.com>
2023-05-13 13:21:37 -0600

    Preload models + better interface
    Still needs some work.

--- gui.py
+++ gui.py
@@ -4,8 +4,12 @@ from streamlit_extras.colored_header import colored_header
 from streamlit_extras.add_vertical_space import add_vertical_space
 from startLLM import main as startLLM
 
+# Initialization
+if "visibility" not in st.session_state:
+    st.session_state.visibility = "visible"
+    st.session_state.running = False
+
 st.set_page_config(page_title="CASALIOY")
-input_text = None
 
 # Sidebar contents
 with st.sidebar:
@@ -36,41 +40,36 @@ input_container = st.container()
 colored_header(label='', description='', color_name='blue-30')
 response_container = st.container()
 
-if "disabled" not in st.session_state:
-    st.session_state["disabled"] = False
-
-def enable():
-    st.session_state["disabled"] = False
-
-def disable():
-    st.session_state["disabled"] = True
-
 # User input
 ## Function for taking user provided prompt as input
-def get_text():
-    global input_text
-    input_text = st.text_input("You: ", "", key="input", disabled=st.session_state["disabled"])
-    return input_text
+#def get_text():
+    #return input_text
+
 ## Applying the user input box
-with input_container:
-    user_input = get_text()
+#with input_container:
+    #user_input = get_text()
 
 # Response output
 ## Function for taking user prompt as input followed by producing AI generated responses
 def generate_response(prompt):
-    #input_text.disabled = True
-    disable()
-    response = startLLM(prompt, True)
-    return response
-
-## Conditional display of AI generated responses as a function of user provided prompts
-with response_container:
-    if user_input:
-        response = generate_response(user_input)
-        st.session_state.past.append(user_input)
+    if prompt.strip() != "":
+        st.session_state.running = True
+        response = startLLM(prompt, True)
+        st.session_state.running = False
+        st.session_state.past.append(prompt)
         st.session_state.generated.append(response)
-        
-    if st.session_state['generated']:
-        for i in range(len(st.session_state['generated'])):
-            message(st.session_state['past'][i], is_user=True, key=str(i) + '_user')
-            message(st.session_state["generated"][i], key=str(i))
+        return response
+
+with st.form("my_form", clear_on_submit=True):
+## Conditional display of AI generated responses as a function of user provided prompts
+    user_input = st.text_input("You: ", "", key="input", disabled=st.session_state.running)
+    st.form_submit_button('SUBMIT', on_click=generate_response(user_input), disabled=st.session_state.running)
+#if user_input:
+    #response = generate_response(user_input)
+    #st.session_state.past.append(user_input)
+    #st.session_state.generated.append(response)
+    
+if st.session_state['generated']:
+    for i in range(len(st.session_state['generated'])):
+        message(st.session_state['past'][i], is_user=True, key=str(i) + '_user')
+        message(st.session_state["generated"][i], key=str(i))
