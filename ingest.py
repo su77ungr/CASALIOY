20bb25139237db0a75ec30b290cbb9e14e0ffefa
Hippalectryon <6502864+hippalectryon-0@users.noreply.github.com>
2023-05-14 21:22:48 +0200

    fix ingest.py (#37)
    * added list to .loader -> multiple files support
    
    ---------
    
    Co-authored-by: su77ungr <69374354+su77ungr@users.noreply.github.com>

--- ingest.py
+++ ingest.py
@@ -1,8 +1,10 @@
+"""ingest documents into vector database using embedding"""
 import os
 import shutil
 import sys
 
 from dotenv import load_dotenv
+from langchain.docstore.document import Document
 from langchain.document_loaders import CSVLoader, PDFMinerLoader, TextLoader, UnstructuredEPubLoader, UnstructuredHTMLLoader
 from langchain.embeddings import LlamaCppEmbeddings
 from langchain.text_splitter import RecursiveCharacterTextSplitter
@@ -15,8 +17,26 @@ documents_directory = os.environ.get("DOCUMENTS_DIRECTORY")
 model_n_ctx = os.environ.get("MODEL_N_CTX")
 
 
-## enables to run python random_path/ to ingest // or 'python random_path/ y' to purge existing db
-def main(sources_directory, cleandb):
+def load_one_doc(filepath: str) -> list[Document]:
+    """load one document"""
+    if filepath.endswith(".txt"):
+        loader = TextLoader(filepath, encoding="utf8")
+    elif filepath.endswith(".pdf"):
+        loader = PDFMinerLoader(filepath)
+    elif filepath.endswith(".csv"):
+        loader = CSVLoader(filepath)
+    elif filepath.endswith(".epub"):
+        loader = UnstructuredEPubLoader(filepath)
+    elif filepath.endswith(".html"):
+        loader = UnstructuredHTMLLoader(filepath)
+    else:
+        raise ValueError(f"Unhandled file format: .{filepath.split('.')[-1]} in {filepath}")
+
+    return loader.load()
+
+
+def main(sources_directory: str, cleandb: str) -> None:
+    """enables to run python random_path/ to ingest // or 'python random_path/ y' to purge existing db"""
     db_dir = persist_directory  # can be changed to ":memory:" but is not persistant
     if os.path.exists(db_dir):
         if cleandb.lower() == "y" or (cleandb == "n" and input("\nDelete current database?(Y/N): ").lower() == "y"):
@@ -25,26 +45,17 @@ def main(sources_directory, cleandb):
         elif cleandb.lower() == "n":
             print("Adding to db...")
 
+    documents = []
     for root, dirs, files in os.walk(sources_directory):
         for file in files:
-            if file.endswith(".txt"):
-                loader = TextLoader(os.path.join(root, file), encoding="utf8")
-            elif file.endswith(".pdf"):
-                loader = PDFMinerLoader(os.path.join(root, file))
-            elif file.endswith(".csv"):
-                loader = CSVLoader(os.path.join(root, file))
-            elif file.endswith(".epub"):
-                loader = UnstructuredEPubLoader(os.path.join(root, file))
-            elif file.endswith(".html"):
-                loader = UnstructuredHTMLLoader(os.path.join(root, file))
+            documents += load_one_doc(os.path.join(root, file))
 
-    documents = loader.load()
     text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=50)
     texts = text_splitter.split_documents(documents)
     llama = LlamaCppEmbeddings(model_path=llama_embeddings_model, n_ctx=model_n_ctx)
     qdrant = Qdrant.from_documents(texts, llama, path=db_dir, collection_name="test")
     qdrant = None
-    print("Indexed ", len(texts), " documents in Qdrant")
+    print(f"Indexed {len(texts)} chunks from {len(documents)} documents in Qdrant")
 
 
 if __name__ == "__main__":
